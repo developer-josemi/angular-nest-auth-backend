6fd4fca254b39a44802d963d12ae8c79b75c9eb6
developer-josemi <jose.martin@nter.es>
2025-01-17 12:30:33 +0100

    vercel

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import * as compression from 'compression'; // Importa el paquete de compresión
-
 import { AppModule } from './app.module';
 
+let app;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  app = await NestFactory.create(AppModule);
 
-  // Habilita CORS
   app.enableCors();
-
-  // Habilita compresión HTTP
-  app.use(compression());
-
-  // Configura el ValidationPipe global
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -21,9 +15,14 @@ async function bootstrap() {
     }),
   );
 
-  // Escucha en el puerto especificado por las variables de entorno o en el 3000
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
-  console.log(`App running on port ${port}`);
+  await app.init();
 }
+
 bootstrap();
+
+export default async (req, res) => {
+  if (!app) {
+    await bootstrap();
+  }
+  app.getHttpAdapter().getInstance()(req, res);
+};
